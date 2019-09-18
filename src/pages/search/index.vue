96790eb00e284187622326e7934f722e1041a3fc
陈工 <chenhonzhou@gmail.com>
2019-09-18 23:39:51 +0800

    搜索

--- src/pages/search/index.vue
+++ src/pages/search/index.vue
@@ -20,9 +20,32 @@
     </view>
     <view class="cu-bar bg-white solid-bottom">
       <view class="margin-left-sm action"><text class="cuIcon-title text-orange"></text>  搜索热词 </view> 
+      <view class="action">
+        <button class="cu-btn bg-green shadow" v-if="searchHotWordsFlag" @click="nextWords(0)">换一批</button>
+      </view>
+    </view>
+    <view class="cu-list grid col-4 no-border">
+      <view class="cu-item text-sm"
+      :class="[ item.color ]"
+      v-for="item in activeSearchHotWords" 
+      :key="item.word">
+        {{ item.word }}
+      </view>
     </view>
     <view class="cu-bar bg-white solid-bottom">
-      <view class="margin-left-sm action"><text class="cuIcon-title text-orange"></text>  热门搜索 </view> 
+      <view class="margin-left-sm action"><text class="cuIcon-title text-orange"></text>  热门搜索 </view>
+      <view class="action">
+        <button class="cu-btn bg-green shadow" v-if="newHotWordsFlag" @click="nextWords(1)">换一批</button>
+      </view>
+    </view>
+    <view class="cu-list grid col-4 no-border">
+      <!-- newHotWords -->
+      <view class="cu-item text-sm"
+      :class="[ item.color ]"
+      v-for="item in activeNewHotWords" 
+      :key="item.word">
+        {{ item.word }}
+      </view>
     </view>
     <view class="cu-bar bg-white solid-bottom" v-if="historys.length">
       <view class="margin-left-sm action"><text class="cuIcon-title text-orange"></text>  搜索历史 </view>
@@ -75,12 +98,6 @@
   const randColr = require("@/utils/toy/randColors")
   import books from "@/utils/api/books/site/zhuishushenqi"
   const nw = new books
-  nw.searchHotWords().then(r=> {
-    console.log(r)
-  })
-  // nw.hotWords().then(r=> {
-  //   console.log(r)
-  // })
 
   export default {
     data() {
@@ -95,14 +112,70 @@
           body: ``
         },
         minBook: cp || cp1,
-        minText: `没有搜索历史记录`
+        minText: `没有搜索历史记录`,
+        newHotWords: [], // 热门搜索, 有id
+        searchHotWords: [],
+        newHotWordsFlag: true,
+        searchHotWordsFlag: true,
+        activeSearchHotWords: [],
+        activeNewHotWords: [],
+        nextFetch: [ [ 0, 4, 4 ], [0, 8, 8] ], // 随机截取
+        history_len: 9, // 搜索历史长度
       }
     },
     onLoad() {
       this.fetchSearchHistorys()
+      this.toFetchHots()
     },
     methods: {
+      nextWords(flag){
+        /* 
+        ** @tips 下一批 
+        ** @param <num> - flag (判断是搜索热词还是热门搜索)
+        ** @return <num>
+        */
+       const that = this
+       const temp = this.nextFetch[flag]
+       const len = temp[1]
+       // 获取当前列表的长度
+       const max_len = flag ? that.searchHotWords.length : that.newHotWords.length
+       // 拿到相加后的值
+       let currentLen = len + temp[2]
+       // 判断相加后的值与列表长度
+       if (currentLen >= max_len) {
+         flag ? that.searchHotWordsFlag = false
+         : that.searchHotWordsFlag = false
+         currentLen = max_len
+       }
+       if (!flag) {
+        that.activeSearchHotWords = that.searchHotWords[len, currentLen]
+       } else {
+        that.activeNewHotWords = that.newHotWords[len, currentLen]
+       }
+       console.log(`y: `, flag, `len: `, len, `current len: `, currentLen, `max_len: `, max_len)
+      },
+      toFetchHots(){
+        const that = this
+        const MapColr = arr=> arr.map(item=> {
+          item.color = randColr(`text`)
+          return item
+        })
+        const ArrSlice = (arr, len)=> arr.slice( 0, len )
+        
+        Promise.all([
+          nw.searchHotWords(),
+          nw.hotWords()
+        ]).then(r=> {
+          const v1 = r[0]
+          const v2 = r[1]
+          that.searchHotWords = MapColr(v1.searchHotWords)
+          that.activeSearchHotWords = ArrSlice(that.searchHotWords, that.nextFetch[0][2])
+          that.newHotWords = MapColr(v2.newHotWords)
+          that.activeNewHotWords = ArrSlice(that.newHotWords, that.nextFetch[1][2])
+        })
+      },
       toText(index) {
+        // 点击文字
         const data = this.historys[index]
         this.search = data.text
         this.setSearchHistorys(data)
@@ -123,7 +196,7 @@
         this.historys = obj
       },
       async setSearchHistorys(data) {
-        const LIMIT = 9
+        const LIMIT = this.history_len
         const old = await this.fetchSearchHistorys(true)
         const len = old.length
         // console.log(`current array: `, old)
