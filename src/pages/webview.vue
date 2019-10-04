9cba9b277c1b2c55d1ceece9770748a30457e784
陈工 <chenhonzhou@gmail.com>
2019-10-04 19:29:43 +0800

    添加`<web-view>`组件

---
+++ src/pages/webview.vue
@@ -0,0 +1,16 @@
+<template>
+  <view class="wrap">
+    <web-view src=""></web-view>
+  </view>
+</template>
+
+<script>
+import qs from 'querystring'
+console.log(qs)
+export default {
+  onLoad(options) {
+    const { title, url } = options
+    uni.setNavigationBarTitle({ title })
+  }
+}
+</script>
